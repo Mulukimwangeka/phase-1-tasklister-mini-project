a4f6bc48bd0869472a57eb781a14f968e97cc82c
mulukimwangeka <mwangekaruth@gmail.com>
2022-08-26 17:37:50 +0300

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ document.querySelector('form').addEventListener('submit',(e)=>{
+  e.preventDefault();
+  const newItem = document.getElementById("new-task-description").value 
+  console.log(newItem);
+  buildtask(newItem);
+
+ })
+  
 });
+function buildtask (task){
+const li =document.createElement("li")
+li.textContent =task
+
+const button =document.createElement("button")
+button.addEventListener('click',handledelete);
+button.textContent = 'X';
+
+li.appendChild(button)
+const theList =document.querySelector('#tasks')
+theList.appendChild(li)
+}
+
+function handledelete(e){
+  e.target.parentNode.remove();
+}
